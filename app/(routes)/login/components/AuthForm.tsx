eaaf82070e7063d8dc9c0b85b2400dcbb0654461
aryan <aryanmoradi74@gmail.com>
2024-04-15 01:51:40 +0300

    Update OAuth redirect URL in AuthForm
    The OAuth redirect URL in the signInWithOAuth function of the AuthForm component has been updated. Instead of using "location.origin" for the base URL, it now uses the "NEXT_PUBLIC_HOST" environment variable. This provides a more dynamic and configurable redirect URL.

--- app/(routes)/login/components/AuthForm.tsx
+++ app/(routes)/login/components/AuthForm.tsx
@@ -11,7 +11,7 @@ export default function AuthForm() {
         await supabase.auth.signInWithOAuth({
             provider: "google",
             options: {
-                redirectTo: `${location.origin}/login/callback`,
+                redirectTo: `${process.env.NEXT_PUBLIC_HOST}/login/callback`,
             },
         });
     };
