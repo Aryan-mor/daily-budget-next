633b79463164891eb0af8a09864f9536a6e94447
aryan <aryanmoradi74@gmail.com>
2024-04-15 01:31:32 +0300

    Remove redundant React components
    The code updates deleted several React components that were redundant and unrequired. This action was carried out to declutter the codebase and optimize the performance of the application. The files deleted include components such as 'button.tsx', 'form.tsx', 'input.tsx', 'label.tsx', and 'tabs.tsx'.

--- app/_components/toast/use-toast.ts
+++ app/_components/toast/use-toast.ts
@@ -1,10 +1,7 @@
 // Inspired by react-hot-toast library
 import * as React from "react"
 
-import type {
-    ToastActionElement,
-    ToastProps,
-} from "@/_components/toast/toast"
+import type {ToastActionElement, ToastProps,} from "@/app/_components/toast/toast"
 
 const TOAST_LIMIT = 1
 const TOAST_REMOVE_DELAY = 1000000
