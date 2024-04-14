6376b00252e91577a4a6a7a3c487ac7691b56c25
aryan <aryanmoradi74@gmail.com>
2024-04-15 01:58:19 +0300

    Update URL retrieval method in login callback route
    Changed the method of retrieving the URL in both instances within the login callback route. The code now first attempts to retrieve the URL from an environment variable, `NEXT_PUBLIC_HOST`, as a replacement for directly picking it from the request object. This can provide more flexibility and control over the referred URL, especially in diversified deployment scenarios.

--- app/(routes)/login/callback/route.ts
+++ app/(routes)/login/callback/route.ts
@@ -3,14 +3,14 @@ import {NextResponse} from "next/server";
 import {cookies} from "next/headers";
 
 export async function GET(request: Request) {
-    const requestUrl = new URL(request.url);
+    const requestUrl = new URL(process.env.NEXT_PUBLIC_HOST ?? request.url);
     const isAuth = cookies().get("supabase-auth-token");
 
     if (isAuth) {
         return NextResponse.redirect(requestUrl.origin);
     }
 
-    const {searchParams} = new URL(request.url);
+    const {searchParams} = new URL(process.env.NEXT_PUBLIC_HOST ?? request.url);
     const code = searchParams.get("code");
 
     if (code) {
